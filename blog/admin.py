c3ec7e6a7f1ed33768399e5872e126f8e60a35b3
Alamin-Tokee <alamintokee@gmail.com>
2023-02-24 16:38:19 +0600

    Updated admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
+#Blog application imports
+from .models.articles_models import Article
+from .models.category_models import Category
+from .models.comment_models import Comment
+from .models.author_models import Profile
+
 # Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_filter = ('user',)
+    search_fields = ('user',)
+    ordering = ['user',]
+
+admin.site.register(Profile, ProfileAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'slug', 'image', 'approved')
+    list_filter = ('name', 'approved',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug':('name', )}
+    ordering = ['name',]
+
+admin.site.register(Category, CategoryAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+    
+    list_display = ('category', 'title', 'slug', 'author', 'image', 'image_credit', 'body', 'date_published', 'status')
+    list_filter = ('status', 'date_created', 'date_published', 'author', )
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'date_published'
+    ordering = ['status', '-date_created']
+    readonly_fields = ('views', 'count_words', 'read_times')
+
+admin.site.register(Article, ArticleAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'email', 'comment', 'article', 'date_created', )
+    list_filter = ('date_created', 'name')
+    search_fields = ('name', 'article', 'comment')
+    date_hierarchy = 'date_created'
+    ordering = ['-date_created', ]
+    readonly_fields = ('name', 'email', 'comment', 'article', 'date_created', 'date_updated')
+
+admin.site.register(Comment, CommentAdmin)
+
+
+
+
